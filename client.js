76ceeeaef77032373cfce071a6ef14208b17b36c
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-01-19 23:59:43 -0600

    feat: add base functionality

--- client.js
+++ client.js
@@ -1,10 +1,39 @@
 const socket = io("http://localhost:3000");
 
 const messageContainerEl = document.getElementById("message-container");
+const messageFormEl = document.getElementById("send-container");
+const messageInputEl = document.getElementById("message-input");
 
-socket.on("chat-message", (data) => {
+const username = prompt("What is your name?", "");
+appendMessage("You joined");
+
+socket.emit("new-user", username);
+
+socket.on("chat-message", ({ message, username }) => {
+  appendMessage(`${username}: ${message}`);
+});
+
+socket.on("user-connected", (username) => {
+  appendMessage(`User connected: ${username}`);
+});
+
+socket.on("user-disconnected", (username) => {
+  appendMessage(`User disconected: ${username}`);
+});
+
+messageFormEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const message = messageInputEl.value.trim();
+
+  appendMessage(`You: ${message}`);
+  socket.emit("chat-message", message);
+  messageInputEl.value = "";
+});
+
+function appendMessage(message) {
   const div = document.createElement("div");
-  div.innerText = data;
+  div.innerText = message;
 
   messageContainerEl.append(div);
-});
+}
--- server.js
+++ server.js
@@ -5,6 +5,23 @@ const io = require("socket.io")(3000, {
   },
 });
 
+const store = {};
+
 io.on("connection", (socket) => {
-  socket.emit("chat-message", "Hello World");
+  socket.on("chat-message", (message) => {
+    socket.broadcast.emit("chat-message", {
+      message,
+      username: store[socket.id],
+    });
+  });
+
+  socket.on("new-user", (username) => {
+    store[socket.id] = username;
+    socket.broadcast.emit("user-connected", username);
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected", store[socket.id]);
+    delete store[socket.id];
+  });
 });
