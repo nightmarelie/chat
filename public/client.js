adfd2ec7dc33de7e20b5358473348895f96e27f2
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-01-20 20:25:32 -0600

    feat: add rooms

--- public/client.js
+++ public/client.js
@@ -3,11 +3,34 @@ const socket = io("http://localhost:3000");
 const messageContainerEl = document.getElementById("message-container");
 const messageFormEl = document.getElementById("send-container");
 const messageInputEl = document.getElementById("message-input");
+const roomContainerEl = document.getElementById("room-container");
 
-const username = prompt("What is your name?", "");
-appendMessage("You joined");
+if (messageFormEl != null) {
+  const username = prompt("What is your name?", "");
+  appendMessage("You joined");
+  socket.emit("new-user", { roomName, username });
 
-socket.emit("new-user", username);
+  messageFormEl.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const message = messageInputEl.value.trim();
+
+    appendMessage(`You: ${message}`);
+    socket.emit("chat-message", { roomName, message });
+    messageInputEl.value = "";
+  });
+}
+
+socket.on("room-created", (room) => {
+  const divEl = document.createElement("div");
+  divEl.innerText = room;
+  const anchorEl = document.createElement("a");
+  anchorEl.href = `/${room}`;
+  anchorEl.innerText = "Join";
+
+  roomContainerEl.append(divEl);
+  roomContainerEl.append(anchorEl);
+});
 
 socket.on("chat-message", ({ message, username }) => {
   appendMessage(`${username}: ${message}`);
@@ -19,16 +42,6 @@ socket.on("user-connected", (username) => {
 
 socket.on("user-disconnected", (username) => {
   appendMessage(`User disconected: ${username}`);
-});
-
-messageFormEl.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const message = messageInputEl.value.trim();
-
-  appendMessage(`You: ${message}`);
-  socket.emit("chat-message", message);
-  messageInputEl.value = "";
 });
 
 function appendMessage(message) {
--- server.js
+++ server.js
@@ -7,14 +7,29 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-const rooms = {};
+const rooms = { name: {} };
 
 app.get("/", (req, res) => {
   res.render("index", { rooms });
 });
 
+app.post("/room", (req, res) => {
+  const room = req.body.room;
+  if (rooms[room] != null) {
+    return res.redirect("/");
+  }
+  rooms[room] = { users: {} };
+  res.redirect(room);
+
+  io.emit("room-created", room);
+});
+
 app.get("/:room", (req, res) => {
-  res.render("room", { roomName: req.params.room });
+  const room = req.params.room;
+  if (rooms[room] == null) {
+    return res.redirect("/");
+  }
+  res.render("room", { roomName: room });
 });
 
 server.listen(3000, () => console.log("listen on the port: 3000"));
@@ -26,23 +41,36 @@ const io = require("socket.io")(server, {
   },
 });
 
-const store = {};
-
 io.on("connection", (socket) => {
-  socket.on("chat-message", (message) => {
-    socket.broadcast.emit("chat-message", {
+  socket.on("chat-message", ({ message, roomName }) => {
+    socket.to(roomName).emit("chat-message", {
       message,
-      username: store[socket.id],
+      username: rooms[roomName].users[socket.id],
     });
   });
 
-  socket.on("new-user", (username) => {
-    store[socket.id] = username;
-    socket.broadcast.emit("user-connected", username);
+  socket.on("new-user", ({ username, roomName }) => {
+    socket.join(roomName);
+    rooms[roomName].users[socket.id] = username;
+    socket.to(roomName).emit("user-connected", username);
   });
 
   socket.on("disconnect", () => {
-    socket.broadcast.emit("user-disconnected", store[socket.id]);
-    delete store[socket.id];
+    getUsersRoom(socket).forEach((roomName) => {
+      socket.broadcast.emit(
+        "user-disconnected",
+        rooms[roomName].users[socket.id]
+      );
+      delete rooms[roomName].users[socket.id];
+    });
   });
 });
+
+function getUsersRoom(socket) {
+  return Object.entries(rooms).reduce((names, [name, room]) => {
+    if (room.users && room.users[socket.id] != null) {
+      names.push(name);
+    }
+    return names;
+  }, []);
+}
