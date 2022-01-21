920371de18cc6cb48fd99ba8c4a4d9f978940e18
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-01-20 18:58:52 -0600

    feat(server): add express and view engine ejs

--- server.js
+++ server.js
@@ -1,4 +1,25 @@
-const io = require("socket.io")(3000, {
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+const rooms = {};
+
+app.get("/", (req, res) => {
+  res.render("index", { rooms });
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomName: req.params.room });
+});
+
+server.listen(3000, () => console.log("listen on the port: 3000"));
+
+const io = require("socket.io")(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
